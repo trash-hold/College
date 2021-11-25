ee76b98eaab1c767ff9b8158755c3194d69508d3
trash-hold <94200155+trash-hold@users.noreply.github.com>
2021-11-25 11:37:22 +0100

    Update def.c

--- checkers/def.c
+++ checkers/def.c
@@ -1,1 +1,28 @@
+#ifndef DEF
 
+#define DEF
+
+#define N 8
+typedef enum {C, W, B, DW, DB} field_type; // C-clear, W - white, B- black , DW - lvluped white, DB - lvleduped black
+
+typedef struct
+{
+    field_type field[N][N];
+    int turn;       //whos turn;  1 - White, 0 - Black; 
+    int capture;    //are there captures
+}Board; 
+
+typedef struct 
+{
+    int x;
+    int y; 
+}field;
+
+typedef struct
+{
+    field from; 
+    field to; 
+}move; 
+
+
+#endif
