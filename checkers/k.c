72dc2264eb2e4e9d38ee11b554746aaee34ae5b3
trash-hold <94200155+trash-hold@users.noreply.github.com>
2022-01-15 14:34:09 +0100

    Update k.c

--- checkers/k.c
+++ checkers/k.c
@@ -4,53 +4,26 @@
 
 void main()
 {
-    //Board A;
-    generate(&A); 
-    A.field[4][4] = B;
-    A.field[1][7] = C;
-    /*move x = {(field){5,1}, (field){3,3}};
-    
-    A.field[4][2] = B;
-    A.field[4][4] = DB;
-    legal_moves *l = create_list(&A);
-    show_board(&A);
-    play();
-    A.turn = 0;
-    show_board(&A);
-    play();
-    show_board(&A);
-    show_list(l);
-    
-    printf("game: %d \n", game_state(&A));*/
-    
-    int Game_state = 0;
-    //game with AI
-    while(Game_state != LOST && Game_state!= WON)
+    generate(&A);
+    printf("Welcome to checkers!");
+    settings = set_up();
+    while(settings.game != Quit)
     {
-        show_board(&A);
-        legal_moves *head = create_list(&A);
-        if(A.turn == 1)
-        {
-            char p1_move[5];
-            gets(p1_move);
-            move p1 = trans_move(p1_move);
-            
-            change_turn(&A, p1);
-            /*int correct = is_correct(&A, trans_move(p1_move));
-            if(correct)
-            {
-                move p1 = trans_move(p1_move);
-                make_move(&A, head, p1);
-                //Have to check if there was a capture and if there is another one avaible
-                if(correct == 2)
-                {
-                    if(!is_other_capture(&A, p1.to)) A.turn = 0;
-                }
-                else A.turn = 0;
-            }*/
-        }
-        else {play();}
-        Game_state = game_state(&A);
+        if(settings.game == PvP) 
+            game_PvP();
+        else if(settings.game == AI) game_with_AI();
+        else game_AI();
+        settings = set_up();
     }
 }
 
+/*
+1. make_move maybe can remove head? and do create_list; probably should put the if for finding queens in the if(t)
+2. rewritting showing board and generating
+3. FREEING SPACE
+
+Bugs: bicie do tyłu?
+      wyświetlanie złego zwyciężcy maybe fixed
+
+Możliwość wielokrotnego bicia!!!
+*/
